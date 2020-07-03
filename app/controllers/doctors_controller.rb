b133571c372b5638e6bdbb69b066aadf88cef1a4
Megan Horsfield <meganthadev@gmail.com>
2020-07-03 06:49:04 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,20 @@
 class DoctorsController < ApplicationController
+  def new 
+  end 
+  
+  def create 
+  end 
+  
+  def show 
+  end 
+  
+  def index 
+    @appointments = Appointment.all 
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end  
 end
