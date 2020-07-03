e596405fa59758730b8c6aa92e225631abd8ddea
Megan Horsfield <meganthadev@gmail.com>
2020-07-03 07:10:41 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,29 +1,5 @@
 class AppointmentsController < ApplicationController
-  before_action :set_appointment!, only: [:show, :edit, :update]
-  def new 
-  end 
-  
-  def create 
-  end 
-  
-  def show 
-  end 
-  
-  
-  def edit 
-  end 
-  
-  def update 
-  end   
-  
-  private
-
-  def appointment_params
-    params.permit(:appointment_datetime)
-  end
-  
-  def set_appointment!
+  def show
     @appointment = Appointment.find(params[:id])
   end
-  
 end
