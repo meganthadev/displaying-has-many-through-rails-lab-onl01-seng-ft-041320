b106eb065ed8c5612553f9664856aff8c59fc903
Megan Horsfield <meganthadev@gmail.com>
2020-07-03 07:22:55 +0000

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,31 @@
 class DoctorsController < ApplicationController
+  before_action :set_doctor!, only: [:show, :index, :edit, :update]
+  def new 
+  end 
+  
+  def create 
+  end 
+  
+  def show 
+  end 
+  
+  def index 
+    @doctors = Doctor.all 
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end  
+  
+  private
+
+  def doctor_params
+    params.permit(:name, :department)
+  end
+  
+  def set_doctor!
+    @doctor = Doctor.find(params[:id])
+  end
 end
