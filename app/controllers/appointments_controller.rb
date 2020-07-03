9cf096a59105bf58ab89b8ace4719869afcb470e
Megan Horsfield <meganthadev@gmail.com>
2020-07-03 06:29:34 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,20 @@
 class AppointmentsController < ApplicationController
+  def new 
+  end 
+  
+  def create 
+  end 
+  
+  def show 
+  end 
+  
+  def index 
+    @appointments = Appointment.all 
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end   
 end
