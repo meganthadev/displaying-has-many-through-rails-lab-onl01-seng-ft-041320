9e286290e04fc553be27606cd70062e9642600d4
Megan Horsfield <meganthadev@gmail.com>
2020-07-03 07:18:25 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,25 +1,9 @@
 class PatientsController < ApplicationController
-
+  def index
+    @patients = Patient.all
+  end
   
-  def new 
-  end 
-  
-  def create 
-  end 
-  
-  def show 
+  def show
     @patient = Patient.find(params[:id])
-  end 
-  
-  def index 
-    @patients = Patient.all 
-  end 
-  
-  def edit 
-  end 
-  
-  def update 
-  end  
-  
-
+  end
 end
